198fd35140a179cc13f0c02e60349e3fd5977fc2
klammert <klammert@weps.cz>
2019-02-16 21:07:07 +0100

    [MOD] added port

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template
 from plot_it import *
 from bokeh.embed import components
@@ -16,4 +17,5 @@ def main():
 
 
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
