de8f864ad1509a14444f5f690138744b5603ea0a
klammert <klammert@weps.cz>
2019-02-17 07:35:36 +0100

    initial commit

--- plot_it.py
+++ plot_it.py
@@ -14,14 +14,14 @@ mapper = linear_cmap(field_name='left',
 
 TOOLTIPS = [
     # ("(x,y)", "($x, $y)"),
-    ('Description', '@Description'),
-    ('Uselessness score', sample_score_perc),
+    ('Popis', '@Popis'),
+    ('Tvé skóre zbytečnosti', sample_score_perc),
 ]
 
 source = ColumnDataSource(data=dict(
     x=x_loc,
-    Uselessness_score=sample_score,
-    Description=['Your uselessness score']
+    Skore_zbytecnosti=sample_score,
+    Popis=['Tvé skóre_zbytečnosti']
 ))
 
 hover_tool = HoverTool(tooltips=TOOLTIPS, renderers=[])
@@ -29,14 +29,14 @@ tools = [hover_tool, WheelZoomTool(), PanTool(), ResetTool()]
 
 plot = plotting.figure(plot_width=800, plot_height=400, tooltips=TOOLTIPS,
                        tools=tools,
-                       title='Uselessness distribution in population')
+                       title='Distribuce zbytečnosti v populaci')
 
 # distribution
 plot.quad(top=hist, bottom=0.00, left=edges[:-1], right=edges[1:],
           color=mapper, alpha=0.5)
 
-point = plot.circle('x', 'Uselessness_score', size=10, source=source,
-                    fill_color='red', legend='Your position')
+point = plot.circle('x', 'Skore_zbytecnosti', size=10, source=source,
+                    fill_color='red', legend='Tvá pozice')
 
 hover_tool.renderers.append(point)
 
@@ -45,6 +45,6 @@ plot.xgrid.visible = False
 plot.ygrid.visible = False
 plot.xaxis.visible = False
 plot.toolbar_location = None
-plot.yaxis.axis_label = 'Uselessness_score'
+plot.yaxis.axis_label = 'Skóre_zbytečnosti'
 plot.legend.location = 'top_left'
 plot.legend.click_policy = 'hide'
